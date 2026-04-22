d7eaad6e22b07d2f86b816d8d21b912a6f56a5a0
Keidson Roby <keidsondesigner@gmail.com>
2026-04-22 11:44:12 -0400

    feat(repository): Created Repository ProductById

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -47,3 +47,28 @@ func (p *ProductRepository) GetProducts() ([]model.Product, error) {
 	// Se não deu erro, retornar todos os produtos
 	return products, nil
 }
+
+// GetProductById executa a query no banco e retorna o produto pelo ID.
+func (p *ProductRepository) GetProductById(id int) (model.Product, error) {
+	query := `SELECT id, name, description, price FROM product WHERE id = $1`
+	rows, err := p.connection.Query(context.Background(), query, id)
+
+	// Se deu erro ao executar a query, retornar nil e o erro
+	if err != nil {
+		fmt.Println(err)
+		return model.Product{}, err
+	}
+	defer rows.Close()
+
+	var product model.Product
+	for rows.Next() {
+		// Se deu erro ao capturar os dados, retornar nil e o erro
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price); err != nil {
+			fmt.Println(err)
+			return model.Product{}, err
+		}
+	}
+
+	// Se não deu erro, retornar o produto
+	return product, nil
+}
