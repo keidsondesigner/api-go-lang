4ec016a4a86f9ab6cbcdebde71e583a5a0a999dc
Keidson Roby <keidsondesigner@gmail.com>
2026-04-22 10:29:48 -0400

    feat: initialize Gin web server with root / and /ping endpoints in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	server := gin.Default()
+
+	server.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, "OK")
+	})
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Println("Starting server on :8080")
+	server.Run(":8080")
 }
